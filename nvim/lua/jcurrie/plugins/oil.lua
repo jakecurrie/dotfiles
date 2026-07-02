de694104263bc557afc48d56a52c6398a73ca59b
Jake Currie <jake@cashio.ai>
2026-07-01 21:57:36 -0700

    add keymaps for oil.nvim

--- nvim/lua/jcurrie/plugins/oil.lua
+++ nvim/lua/jcurrie/plugins/oil.lua
@@ -3,6 +3,26 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {},
+  keys = {
+    {
+      "-",
+      function()
+        require("oil").open()
+      end,
+      desc = "Open Oil",
+    },
+    {
+      "<leader>-",
+      function()
+        local root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+        if vim.v.shell_error ~= 0 then
+          root = vim.fn.getcwd()
+        end
+        require("oil").open(root)
+      end,
+      desc = "Open Oil at git root",
+    },
+  },
   -- Optional dependencies
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
