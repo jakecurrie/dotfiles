f8b9d4ff92e53ff1a3b148bc35d4482f9c0c8650
Jake Currie <jake@cashio.ai>
2026-07-01 22:25:45 -0700

    add trouble.nvim keymaps

--- nvim/lua/jcurrie/plugins/trouble.lua
+++ nvim/lua/jcurrie/plugins/trouble.lua
@@ -4,16 +4,20 @@ return {
             require("trouble").setup({ })
 
             vim.keymap.set("n", "<leader>tt", function()
-                require("trouble").toggle()
-            end)
+                require("trouble").toggle({ focus = true })
+            end, { desc = "Toggle Trouble" })
+
+            vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle focus=true<cr>", { desc = "Workspace Diagnostics" })
+
+            vim.keymap.set("n", "<leader>xX", "<cmd>Trouble diagnostics toggle focus=true filter.buf=0<cr>", { desc = "Buffer Diagnostics" })
 
             vim.keymap.set("n", "[t", function()
                 require("trouble").next({skip_groups = true, jump = true});
-            end)
+            end, { desc = "Next Trouble Item" })
 
             vim.keymap.set("n", "]t", function()
                 require("trouble").previous({skip_groups = true, jump = true});
-            end)
+            end, { desc = "Previous Trouble Item" })
 
         end
 }
