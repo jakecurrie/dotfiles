3f2667a40f124dbd450545812b06d422e536e92e
Jake Currie <jake@cashio.ai>
2026-07-01 19:29:52 -0700

    add WIP nvim core config files

---
+++ nvim/lua/jcurrie/core/keymaps.lua
@@ -0,0 +1,7 @@
+-- ════════════════════════════════════════════════════════════════════════════
+-- Better Editing
+-- ════════════════════════════════════════════════════════════════════════════
+-- Better indenting (stay in visual mode)
+vim.keymap.set("v", "<", "<gv", { desc = "Indent Left" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent Right" })
+
---
+++ nvim/lua/jcurrie/init.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+require("jcurrie.core.options")
+require("jcurrie.core.keymaps")
+require("jcurrie.core.lazy")
